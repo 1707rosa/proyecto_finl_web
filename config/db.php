a4a646d77652bb8fc37f0b7183b93a13169402dd
Berlyng Yena <berlyngyg@gmail.com>
2025-08-20 18:06:53 -0400

    Arreglando conexion y bug de incidencias

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$db   = "proyectofinalweb"; //aqui se coloca el nombre de la base de datos
+$user = "root";
+$pass = "santa123#"; //si no tiene password dejar en blanco(en mi caso si tengo)
+
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=$db;charset=utf8", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch(PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
+?>
